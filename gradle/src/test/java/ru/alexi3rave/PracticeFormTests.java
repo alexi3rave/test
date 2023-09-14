5b0c90aeca8b9d29ec0ff8618b4914876e3b0cac
Alex Brave <alexi3rave@gmail.com>
2023-09-14 13:11:50 +0300

    added test.png

--- gradle/src/test/java/ru/alexi3rave/PracticeFormTests.java
+++ gradle/src/test/java/ru/alexi3rave/PracticeFormTests.java
@@ -5,19 +5,21 @@ import com.codeborne.selenide.WebDriverRunner;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
+import java.io.File;
+
 import static com.codeborne.selenide.Selenide.*;
 
 public class PracticeFormTests {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.browserSize="1600x900";
+        Configuration.browserSize = "1600x900";
 
     }
+
     @Test
     void fillFormTest() {
-        open ("https://demoqa.com/automation-practice-form");
+        open("https://demoqa.com/automation-practice-form");
 
         System.out.println("Real size: " + WebDriverRunner.getWebDriver().manage().window().getSize());
 
@@ -25,13 +27,22 @@ public class PracticeFormTests {
         $("#lastName").setValue("Boy");
         $("#userEmail").setValue("aaa@aa.aa");
         $("#gender-radio-1").doubleClick();
+        $("#hobbies-checkbox-2").doubleClick();
         $("#userNumber").setValue("1234567890");
-        setDateByName("16.09.2009");
+        setDateByName();
         $("#currentAddress").setValue("shape str, 99");
-                  }
+        new File("gradle/src/test/resources/test.png");
+        $("#uploadPicture").uploadFromClasspath("test.png");
 
-    void setDateByName(String date) {
-        executeJavaScript(String.format("$('[name=\"%s\"]').val('%s')",
-                "#dateOfBirthInput", date));
+        sleep(5000);
     }
+
+    void setDateByName() {
+        executeJavaScript(
+                String.format("$('[name=\"%s\"]').val('%s')",
+                        $("#dateOfBirthInput"), "2007.07.06")
+        );
+
+            }
+
 }
